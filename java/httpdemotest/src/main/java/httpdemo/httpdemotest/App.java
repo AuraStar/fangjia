8e5630759cf5157bf187e1c98e5cdb848bd45015
wangwenyan <wangwenyan@fangjia.com>
2017-12-12 18:07:13 +0800

    init

---
+++ java/httpdemotest/src/main/java/httpdemo/httpdemotest/App.java
@@ -0,0 +1,13 @@
+package httpdemo.httpdemotest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
